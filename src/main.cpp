ae23df06eb519f0f30d0a90772dbd69b7d3955b5
Prajval <seeramprajval0@gmail.com>
2026-06-24 17:43:14 +0530

    Integrate database distance heuristic

--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,60 @@
 #include <iostream>
 
 #include "solver/Solver.h"
-#include "database/StateDatabase.h"
+#include "search/Heuristic.h"
 
 using namespace std;
 
 int main()
 {
-    StateDatabase db;
-
-    db.load("database.bin");
+    Solver solver;
 
     Cube cube;
 
+    // 10-move benchmark
     cube.applyMove(Move::R);
     cube.applyMove(Move::U);
     cube.applyMove(Move::F);
+    cube.applyMove(Move::L);
+    cube.applyMove(Move::D);
+    cube.applyMove(Move::B);
+    cube.applyMove(Move::R);
+    cube.applyMove(Move::U);
+    cube.applyMove(Move::F);
+    cube.applyMove(Move::L);
     cube.applyMove(Move::R);
     cube.applyMove(Move::U);
     cube.applyMove(Move::F);
 
+    Heuristic h;
+
     cout
-        << "Distance: "
-        << db.getDistance(cube)
+        << "Misplaced Stickers: "
+        << h.misplacedStickers(cube)
         << "\n";
+
+    cout
+        << "Heuristic Value: "
+        << h.misplacedStickers(cube) / 4
+        << "\n\n";
+
+    SolveResult result =
+        solver.solve(cube);
+
+    cout
+        << "Solution Length: "
+        << result.solution.size()
+        << "\n";
+
+    cout
+        << "Nodes Expanded: "
+        << result.nodesExpanded
+        << "\n";
+
+    cout
+        << "Solve Time: "
+        << result.solveTimeMs
+        << " ms\n";
 
     return 0;
 }
--- src/search/IDAStar.cpp
+++ src/search/IDAStar.cpp
@@ -3,102 +3,69 @@
 
 using namespace std;
 
-IDAStar::IDAStar(
-    StateDatabase* db
-)
-{
-    database = db;
+IDAStar::IDAStar(StateDatabase* d) {
+    database = d;
 }
 
-bool IDAStar::search(
-    Node node,
-    int threshold,
-    vector<Move>& solution)
-{
+bool IDAStar::search(Node n, int t, vector<Move>& s) {
     stats.nodesExpanded++;
 
-    if(node.depth > 20)
-    {
-        return false;
+    if (n.depth > 20) return false;
+
+    int h = heuristic.misplacedStickers(n.cube) / 4;
+    
+    if (database) {
+        int d = database->getDistance(n.cube);
+        if (d != -1) h = d;
+        else if (h < 7) h = 7;
     }
 
-    int cost =
-        node.depth +
-        heuristic.misplacedStickers(node.cube) / 4;
+    int c = n.depth + h;
 
-    if(cost > threshold)
-    {
-        return false;
+    if (c > t) return false;
+
+    if (n.cube.isSolved()) {
+        s = n.path;
+        return true;
     }
 
-    if(node.cube.isSolved())
-{
-    solution = node.path;
-    return true;
-}
+    if (database && database->contains(n.cube)) {
+        s = n.path;
+        vector<Move> l = database->getSolution(n.cube);
+        s.insert(s.end(), l.begin(), l.end());
+        return true;
+    }
 
-if(database &&
-   database->contains(node.cube))
-{
-    solution = node.path;
+    MoveGenerator m;
+    vector<Node> k = m.generateChildren(n);
 
-    vector<Move> tail =
-        database->getSolution(
-            node.cube
-        );
-
-    solution.insert(
-        solution.end(),
-        tail.begin(),
-        tail.end()
-    );
-
-    return true;
-}
-
-    MoveGenerator generator;
-
-    vector<Node> children =
-        generator.generateChildren(node);
-
-    for(Node child : children)
-    {
-        if(search(child,
-                  threshold,
-                  solution))
-        {
-            return true;
-        }
+    for (Node p : k) {
+        if (search(p, t, s)) return true;
     }
 
     return false;
 }
 
-vector<Move> IDAStar::solve(Node root)
-{
+vector<Move> IDAStar::solve(Node r) {
     stats.nodesExpanded = 0;
 
-    int threshold =
-        heuristic.misplacedStickers(root.cube) / 4;
+    int t = heuristic.misplacedStickers(r.cube) / 4;
+    
+    if (database) {
+        int d = database->getDistance(r.cube);
+        if (d != -1) t = d;
+        else if (t < 7) t = 7;
+    }
 
-    vector<Move> solution;
+    vector<Move> s;
 
-    while(true)
-    {
-        solution.clear();
-
-        if(search(root,
-                  threshold,
-                  solution))
-        {
-            return solution;
-        }
-
-        threshold++;
+    while (true) {
+        s.clear();
+        if (search(r, t, s)) return s;
+        t++;
     }
 }
 
-SearchStats IDAStar::getStats() const
-{
+SearchStats IDAStar::getStats() const {
     return stats;
 }
